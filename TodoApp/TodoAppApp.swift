8dae59859163421e3dfd3287db3b53cd6c587314
RuiOkazaki <okazaki200226@gmail.com>
2021-06-09 20:40:39 +0900

    first commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by 岡崎流依 on 2021/06/09.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
